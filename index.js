2dabb709681bc977ef0d287f600b5c81777226e8
Josakko <112590914+Josakko@users.noreply.github.com>
2023-03-05 02:17:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const { Configuration, OpenAIApi } = require("openai");
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+});
+
+const config = new Configuration({
+    apiKey: process.env.API,
+  });
+
+const openai = new OpenAIApi(config);
+
+client.once('ready', async() => {
+  console.log(`Bot is online, invite link for ${client.user.username}: https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=687195278400&scope=bot`);
+});
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return; 
+    if (message.channel.id !== process.env.CHANNEL) return; 
+    if (message.content.startsWith("!")) return;
+    const prompt = message.content; 
+  
+    const reply = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0.7,
+        max_tokens: 1000,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        }); 
+        message.reply(reply.data.choices[0].text); 
+    });
+
+client.login(process.env.TOKEN);
